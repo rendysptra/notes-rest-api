12b8034ab09e0d6b3be9d203cdace58193dfeb9e
Rendy Syahputra <rendysyhptra2057@gmail.com>
2026-09-14 18:07:49 +0700

    Implement updateNote method in NoteService and NoteServiceImpl for note updates

--- src/main/java/com/rendysaptra/notes/service/NoteService.java
+++ src/main/java/com/rendysaptra/notes/service/NoteService.java
@@ -1,13 +1,17 @@
 package com.rendysaptra.notes.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.rendysaptra.notes.domain.CreateNoteRequest;
+import com.rendysaptra.notes.domain.UpdateNoteRequest;
 import com.rendysaptra.notes.domain.entity.Note;
 
 public interface NoteService {
 
     Note createNote(CreateNoteRequest request);
+
+    Note updateNote(UUID id, UpdateNoteRequest request);
 
     List<Note> listNotes();
 
