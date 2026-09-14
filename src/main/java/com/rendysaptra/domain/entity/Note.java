4532788fde9d6bfc6f9e35218f1fd57abd55b6a3
Rendy Syahputra <rendysyhptra2057@gmail.com>
2026-09-14 13:34:31 +0700

    Add Note entity with fields for id, title, content, url, created, and updated timestamps

---
+++ src/main/java/com/rendysaptra/domain/entity/Note.java
@@ -0,0 +1,130 @@
+package com.rendysaptra.domain.entity;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity 
+@Table(name = "notes")
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "url")
+    private String url;
+
+    @Column(name = "created", updatable = false, nullable = false)
+    private Instant created;
+
+    @Column(name = "updated", nullable = false)
+    private Instant updated;
+
+    public Note(){ 
+    }
+
+    public Note(UUID id, String title, String content, String url, Instant created, Instant updated) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.url = url;
+        this.created = created;
+        this.updated = updated;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Instant getCreated() {
+        return created;
+    }
+
+    public void setCreated(Instant created) {
+        this.created = created;
+    }
+
+    public Instant getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Instant updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Note other = (Note) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Note [id=" + id + ", title=" + title + ", content=" + content + ", url=" + url + ", created=" + created
+                + ", updated=" + updated + "]";
+    }
+
+    
+
+}
